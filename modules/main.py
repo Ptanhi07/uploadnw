da1ddb7dc9e3df73f6565c31a79e2594aa6b848f
Ptanhi07 <mkgamerofficial01@gmail.com>
2025-05-02 13:40:12 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -26,7 +26,7 @@ bot = Client(
     "bot",
     api_id=28466214,
     api_hash= "3f55d44aae0f6c72f0dd8855adeeb60f",
-    bot_token= "7839937380:AAE7yWDTvYGGVcptgwsCf24RR8ClzI257vE")
+    bot_token= "7660015741:AAFHEs9GnwzZmF3duyVMiL-GGVWyhU8QQXI")
 
 
 @bot.on_message(filters.command(["start"]))
